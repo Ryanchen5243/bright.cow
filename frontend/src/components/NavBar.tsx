387cfe7b0048aeb43d0492f4d462cb60e09a53c0
Ryanchen5243 <rchen5243@gmail.com>
2026-05-05 17:20:13 -0400

    foo

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,16 +1,17 @@
-// import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import { AccountCircle } from "@mui/icons-material";
 import { Business } from "@mui/icons-material";
+import { Notifications } from '@mui/icons-material';
 export default function NavBar() {
     return (
         <nav>
             <div className="nav-left">
-                <AccountCircle />
-                <Business />
+                <Business className="nav-icon"/>
                 <p>LobbyNest</p>
             </div>
             <div className="nav-right">
                 <div><input id="search-input" type='text' placeholder='Search games...'/></div>
+                <Notifications className="nav-icon"/>
+                <AccountCircle className="nav-icon"/>
             </div>
         </nav>
     )
