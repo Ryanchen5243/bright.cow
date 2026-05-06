1158d290f8c9981f4adbdd7f1bb583b4ef6a4ffa
Ryanchen5243 <rchen5243@gmail.com>
2026-05-06 01:09:30 -0400

    minor changes to search

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,15 +1,40 @@
 import { AccountCircle } from "@mui/icons-material";
 import { Business } from "@mui/icons-material";
 import { Notifications } from '@mui/icons-material';
+import { Search } from '@mui/icons-material';
+import { useState } from 'react';
 export default function NavBar() {
+    const [openModal, setOpenModal] = useState(false);
+    const [searchValue, setSearchValue] = useState('');
+    const [suggestions] = useState(['league of legends', 'valorant', 'minecraft', 'fortnite', 'apex legends']);
+    
+    
     return (
         <nav>
-            <div className="nav-left">
+            <div className="nav-company-name">
                 <Business className="nav-icon"/>
-                <p>LobbyNest</p>
+                <p>UWU~VIBE</p>
             </div>
-            <div className="nav-right">
-                <div><input id="search-input" type='text' placeholder='Search games...'/></div>
+            <div className="nav-search-container">
+                <div className="nav-search-input" onClick={() => setOpenModal(true)}>
+                    <Search className="search-icon"/>
+                    <input type="text"  placeholder="Search games..." 
+                        value={searchValue} 
+                        onChange={(e) => setSearchValue(e.target.value)} 
+                        onFocus={() => setOpenModal(true)} 
+                        onBlur={() => setTimeout(() => setOpenModal(false), 200)}/>
+                </div>
+                {/* {openModal && (
+                    <div className="search-suggestions-overlay">
+                        {suggestions.filter(suggestion => suggestion.toLowerCase().includes(searchValue.toLowerCase())).map((suggestion, index) => (
+                            <div key={index} className="search-suggestion-item">
+                                <span>{suggestion}</span>
+                            </div>
+                        ))}
+                    </div>
+                )} */}
+            </div>
+            <div className="nav-profile-icon">
                 <Notifications className="nav-icon"/>
                 <AccountCircle className="nav-icon"/>
             </div>
