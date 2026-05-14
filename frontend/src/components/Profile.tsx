4f604ca39fd71650002363cbfc7e727c3a105110
Ryanchen5243 <rchen5243@gmail.com>
2026-05-13 23:39:06 -0400

    profile changes

--- frontend/src/components/Profile.tsx
+++ frontend/src/components/Profile.tsx
@@ -1,6 +1,8 @@
 import bg from '../assets/default_background_photo.jpg';
 import pfp from '../assets/default_profile_photo.jpg';
+import { useState } from 'react';
 export default function Profile() {
+    const [profileTab, setProfileTab] = useState("overview");
     return (
         <div className="profile-view">
             <div className="profile-header" style={{ backgroundImage: `url(${bg})` }}>          
@@ -9,7 +11,7 @@ export default function Profile() {
                         <img src={pfp} alt="Profile avatar" />
                     </div>
                     <div className="profile-user-info">
-                        <h1 className='h1-style'>Emily </h1>
+                        <h1 className='h1-style'>Emily Nova</h1>
                         <p>@emily_gamer</p>
                         <p>Boston, MA, 14:00 UTC</p>
                     </div>
@@ -21,7 +23,25 @@ export default function Profile() {
                 </div>
             </div>
             <div className="profile-main">
-                <h1>other main</h1>
+                {/* <div> games section <br /> games section <br /> games section </div>
+                <div>Schedule section</div>
+                <div>Reviews Section</div> */}
+                <div className="profile-main-tabs">
+                    <span onClick={()=> setProfileTab("overview")}>Overview</span>
+                    <span onClick={()=> setProfileTab("games")}>Games</span>
+                    <span onClick={()=> setProfileTab("schedule")}>Schedule</span>
+                    <span onClick={()=> setProfileTab("media")}>Media</span>
+                </div>
+                {profileTab === "overview" && <div className="profile-user-bio">
+                <p>Born in Washington DC 1992 <br />
+                I am your girl next door <br />
+                looking for a man to play valorant with 😉 <br />
+                if you are interested hit me up <br />
+                Dont be shy</p>
+            </div>}
+                {profileTab === "games" && <h1>games</h1>}
+                {profileTab === "schedule" && <h1>schedule</h1>}
+                {profileTab === "media" && <h1>media</h1>}
             </div>
         </div>
     );
