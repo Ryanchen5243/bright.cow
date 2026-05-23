194feff79319cb312ee2f4d005e7b1b9c9d6c1b0
Ryanchen5243 <rchen5243@gmail.com>
2026-05-23 01:00:12 -0400

    removed three col layout

--- frontend/src/components/AppMain.tsx
+++ frontend/src/components/AppMain.tsx
@@ -1,17 +1,11 @@
 import Profile from "./Profile"
-import RightAside from "./RightAside"
 
 export default function AppMain({appView}: {appView: string}) {
   return (
     <>
       <div className="app-main">
-        {appView === "home" && <h1>Home View</h1>}
-        {appView === "messages" && <h1>Messages View</h1>}
-        {appView === "favorites" && <h1>Favorites View</h1>}
-        {appView === "bookings" && <h1>Bookings View</h1>}
         {appView === "profile" && <Profile />}
       </div>
-      <RightAside appView={appView} />
     </>
   );
 }
