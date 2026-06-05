c9228ce7fac1c7c2f251f693132c787bd2c5f17c
Ryanchen5243 <rchen5243@gmail.com>
2026-06-05 02:38:34 -0400

    draft upcoming

--- frontend/src/components/CreatorSchedule.tsx
+++ frontend/src/components/CreatorSchedule.tsx
@@ -1,6 +1,7 @@
 import { Calendar } from '@calendarjs/react';
 import '@calendarjs/react/style.css';
 import { useState } from 'react';
+import { Schedule, Check } from "@mui/icons-material";
 
 const getLocalDateString = () => {
     const now = new Date();
@@ -21,14 +22,85 @@ export default function CreatorSchedule({ isLoggedIn }: { isLoggedIn: boolean })
         <div className="creator-schedule">
             <div className="schedule-overview">
                 <div className="upcoming-schedule">
-                    <h2>Upcoming Schedule</h2>
-                    {/* Placeholder for upcoming schedule items */}
-                    <p>No upcoming events.</p>
+                    <div className="upcoming-schedule-header">
+                        <Schedule fontSize="large" htmlColor="#9557ED" />
+                        <div>
+                            <h2>Upcoming Availability</h2>
+                            <p>Book a session in the next 24h</p>
+                        </div>
+                    </div>
+                    <div className="upcoming-schedule-contents">
+                        <div className="schedule-item">
+                            <h3>Today</h3>
+                            <div className="schedule-times">
+                                <div>
+                                    <p>7:00 PM</p>
+                                    <p>30 min</p>
+                                    <button>Book</button>
+                                </div>
+                                <div>
+                                    <p>8:00 PM</p>
+                                    <p>30 min</p>
+                                    <button>Book</button>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="schedule-item">
+                            <h3>Tomorrow</h3>
+                            <div className="schedule-times">
+                                <div>
+                                    <p>2:00 PM</p>
+                                    <p>1 hr</p>
+                                    <button>Book</button>
+                                </div>
+                                <div>
+                                    <p>3:00 PM</p>
+                                    <p>30 min</p>
+                                    <button>Book</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    {/* <p>No upcoming events.</p> */}
                 </div>
                 <div className="recent-schedule">
-                    <h2>Past Schedule</h2>
-                    {/* Placeholder for past schedule items */}
-                    <p>No past events.</p>
+                    <div className="recent-schedule-header">
+                        <h2>Recent Activity</h2>
+                        <p>No past events.</p>
+                    </div>
+                    <div className="recent-schedule-contents">
+                        <div className="recent-activity-item">
+                            <Check fontSize="medium" htmlColor="#9557ED" />
+                            <div>
+                                <p>A user booked a Duo Gaming session.</p>
+                                <p>2 min ago</p>
+                            </div>
+                        </div>
+                        <div className="recent-activity-item">
+                            <Check fontSize="medium" htmlColor="#9557ED" />
+                            <div>
+                                <p>Someone from USA booked a Coaching session.</p>
+                                <p>5 min ago</p>
+                            </div>
+                        </div>
+                        <div className="recent-activity-item">
+                            <Check fontSize="medium" htmlColor="#9557ED" />
+                            <div>
+                                <p>A user completed a Chill & Talk session.</p>
+                                <p>15 min ago</p>
+                            </div>
+                        </div>
+                        <div className="recent-activity-item">
+                            <Check fontSize="medium" htmlColor="#9557ED" />
+                            <div>
+                                <p>A user booked a Custom Session</p>
+                                <p>22 min ago</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="recent-schedule-footer">
+                        <p>Live feed updates automatically</p>
+                    </div>
                 </div>
                 <div className="schedule-actions">
                     <button className="add-schedule-btn">Add Schedule</button>
