4ea62ece74b7a95cb5a88af7f3b6b73076b4bde6
ivanzheng123 <ivanzheng456@gmail.com>
2026-08-23 21:03:13 -0400

    got back the progress i lost

---
+++ frontend/src/components/ProfileInformation.tsx
@@ -0,0 +1,191 @@
+import {Add, Edit, Language, PermIdentity, Save} from '@mui/icons-material';
+import { useState } from 'react';
+import {
+  Combobox,
+  ComboboxContent,
+  ComboboxEmpty,
+  ComboboxInput,
+  ComboboxItem,
+  ComboboxList,
+} from "@/components/ui/combobox"
+
+export default function ProfileInformation(){
+    const [isDisabled, setIsDisabled] = useState(true);
+    const [languageOpen, setLanguageOpen] = useState(false);
+    const [selectedLocation, setSelectedLocation] = useState("");
+
+    function addLanguage(){
+        //add logic here to pop out a combo box and add a language
+        setLanguageOpen((prev) => !prev);
+    }
+
+    function allowEdits(){
+        //add logic here to allow the profile information to be edited when clicked
+        const currentState = isDisabled;
+        setIsDisabled(!currentState);
+
+        if(currentState == true){
+            //implement logic to change db
+        }
+    }
+
+    function addingLanguage(location: string){
+        setSelectedLocation(location);
+        setLanguageOpen((prev) => !prev);
+    }
+
+    const displayName = "temp Display Name";
+    const username = "temp username";
+    const bio = "some bio";
+    const defaultLocation = "some location";
+    const birthday = "some birthday";
+    const gender = "a gender";
+    const joinDate = "7/6/67";
+    const locationOptions = ["San Francisco", "New York", "London", "Barcelona", "Remote"];
+
+    return(
+        <div className="settings-profile-container">
+            <h2>Profile Information</h2>
+            <p>Update your public profile information and how others see  you.</p>
+            {/* This is the first panel which has all the profile details and display name etc. */}
+            <div className="profile-panel">
+                {/* The top part of the card which has t he edit button and title/subtitle */}
+                <div className="settings-container-test">
+                    <div >
+                        <div className="settings-profile-alignment">
+                            <PermIdentity className="settings-profile-svg"/>
+                            <h3 className="settings-profile-detail-title">Profile Details</h3>
+                        </div>
+                        <p className="settings-profile-subtitle">Manage how your public profile appears.</p>
+                    </div>
+                    <div className="settings-button-container">
+                        <button className="settings-edit-button" onClick={allowEdits}>
+                            {isDisabled ?
+                                <div className='flex justify-center items-center'><Edit sx={{fontSize: 17}} className='mr-1'/> Edit</div> :
+                                <div className='flex justify-center items-center'><Save sx={{fontSize: 17}} className='mr-1'/> Save</div>
+                            }
+                        </button>
+                    </div>
+                </div>
+                
+                {/* User fields */}
+                <div className="settings-detail-container">
+                    <div className="settings-profile-picture">
+                        <img src="src/assets/default_profile_photo.jpg" className="settings-profile-photo"/>
+                    </div>
+                    <div className="settings-profile-information">
+                        <div className="settings-input-group">
+                            <label htmlFor="display_name" className="settings-input-label">Display name</label>
+                            <input className="settings-detail-one-box" type="text" id="display_name" name="display_name" defaultValue={displayName} disabled={isDisabled}/>
+                        </div>
+
+                        <div className="settings-input-group">
+                            <label htmlFor="username" className="settings-input-label">Username</label>
+                            <input className="settings-detail-one-box" type="text" id="username" name="username" defaultValue={username} disabled={isDisabled}/>
+                        </div>
+
+                        <div className="settings-input-group">
+                            <label htmlFor="bio" className="settings-input-label">Bio</label>
+                            <textarea className="settings-detail-bio" id="bio" name="bio" defaultValue={bio} disabled={isDisabled} maxLength={150} />
+                        </div>
+
+                        <span className="settings-container">
+                            <div className="settings-input-group">
+                                <label htmlFor="location" className="settings-input-label">Location</label>
+                                <Combobox
+                                    items={locationOptions}
+                                    value={selectedLocation}
+                                    onValueChange={setSelectedLocation}
+                                    disabled={isDisabled}
+                                >
+                                    <ComboboxInput
+                                        id="location"
+                                        name="location"
+                                        className="w-full settings-detail-two-box-left"
+                                        placeholder="Select a location"
+                                        disabled={isDisabled}
+                                    />
+                                <ComboboxContent
+                                    className="settings-detail-two-box-left-content"
+                                    style={{ width: "var(--anchor-width, 100%)", minWidth: "var(--anchor-width, 100%)" }}
+                                >
+                                        <ComboboxEmpty>No items found.</ComboboxEmpty>
+
+                                        <ComboboxList>
+                                            {(item) => (
+                                                <ComboboxItem key={item} value={item}>
+                                                    {item}
+                                                </ComboboxItem>
+                                            )}
+                                        </ComboboxList>
+                                    </ComboboxContent>
+                                </Combobox>
+                            </div>
+                            <div className="settings-input-group">
+                                <label htmlFor="gender" className="settings-input-label-right">Gender</label>
+                                <input className="settings-detail-two-box-right" type="text" id="gender" name="gender" placeholder="Pick gender" disabled={isDisabled} defaultValue={gender}/>
+                            </div>
+                        </span>
+
+                        <span className="settings-container">
+                            <div className="settings-input-group">
+                                <label htmlFor="birthday" className="settings-input-label">Birthday</label>
+                                <input className="settings-detail-two-box-left" type="text" id="birthday" name="birthday" placeholder="Enter Birthday" disabled={true} value={birthday}/>
+                            </div>
+                            <div className="settings-input-group">
+                                <label htmlFor="joined" className="settings-input-label-right">Join Date</label>
+                                <input className="settings-detail-two-box-right" type="text" id="joined" name="joined" placeholder="Join Date" disabled={true} value={joinDate}/>
+                            </div>
+                        </span>
+                        
+                        <div className="settings-language-alignment">
+                            <div className="settings-profile-alignment">
+                                <Language className='settings-profile-svg'/>
+                                <h3 className="settings-profile-detail-title">Languages</h3>
+                            </div>
+                            <p className="settings-profile-subtitle">Add the languages you speak.</p>
+
+                            <div className="settings-language-badges">
+                                {/* Maybe add an actual lucide icon here instead of this small + */}
+                                
+                                {languageOpen ? (
+                                    <div className='settings-language-combobox'>
+                                        <Combobox
+                                            items={locationOptions}
+                                            value={selectedLocation}
+                                            onValueChange={addingLanguage}
+                                        >
+                                            {/* TODO: CHANGE THIS COLOR LATER */}
+                                            <ComboboxInput
+                                                id="location"
+                                                name="location"
+                                                className="w-2xs rounded-lg bg-(--vibrant-purple)" 
+                                                placeholder="Select a location"
+                                            />
+                                        {/* TODO: CHANGE THIS COLOR LATER */}
+                                        <ComboboxContent
+                                            className="bg-purple-600 cursor-pointer"
+                                            style={{ width: "var(--anchor-width, 100%)", minWidth: "var(--anchor-width, 100%)" }}
+                                        >
+                                                <ComboboxEmpty>No items found.</ComboboxEmpty>
+
+                                                <ComboboxList>
+                                                    {(item) => (
+                                                        <ComboboxItem key={item} value={item}>
+                                                            {item}
+                                                        </ComboboxItem>
+                                                    )}
+                                                </ComboboxList>
+                                            </ComboboxContent>
+                                        </Combobox>
+                                    </div>
+                                ):
+                                <button className="settings-add-language" onClick={addLanguage}><Add/></button>}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+import path from "path";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   server: {
     proxy: {
       '/allUsers': 'http://localhost:5001',
