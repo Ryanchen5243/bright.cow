c38a83eec6243517d68b0e3a160cfbc60ce3823a
tofu11 <ziangchen1105@gmail.com>
2026-09-01 20:12:56 -0400

    for vincent

--- backend/server.js
+++ backend/server.js
@@ -118,7 +118,7 @@ app.get("/user", (req, res) => {
   res.send("User route");
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 const server = app.listen(PORT, () => {
   console.log(`Server running on ${PORT}`);
 });
